3db9405f24d400b00a59fc038bb95569812c17eb
leandrodzn <leodzib100@gmail.com>
2024-11-18 20:00:43 -0600

    Implement image validation and update logic in WebTemplate controller

--- src/api/admin/web-templates/controllers/index.js
+++ src/api/admin/web-templates/controllers/index.js
@@ -1,4 +1,9 @@
-const { Event, WebTemplate, WebTemplateEvent, Image } = require("../../../../models");
+const {
+  Event,
+  WebTemplate,
+  WebTemplateEvent,
+  Image,
+} = require("../../../../models");
 const { validatorCreateWebTemplate } = require("../validators");
 const { validatorId } = require("../../../../utils/validators");
 const constants = require("../../../../utils/constants");
@@ -27,6 +32,22 @@ const createWebTemplate = async (req, res) => {
 
     back.imageUploadResult = imageUploadResult;
 
+    if (!imageUploadResult?.mainImage) {
+      throw new CustomError({
+        status: 400,
+        name: "BadRequest",
+        message: constants.response.IMAGE_REQUIRED,
+      });
+    }
+
+    if (!imageUploadResult?.additionalImages?.length == 2) {
+      throw new CustomError({
+        status: 400,
+        name: "BadRequest",
+        message: constants.response.IMAGES_REQUIRED,
+      });
+    }
+
     // Validate the data sent from the client
     await validatorCreateWebTemplate(req);
 
@@ -40,7 +61,7 @@ const createWebTemplate = async (req, res) => {
         name,
         link,
         description,
-        price
+        price,
       },
       { transaction }
     );
@@ -172,6 +193,17 @@ const updateWebTemplateById = async (req, res) => {
 
     back.imageUploadResult = imageUploadResult;
 
+    if (
+      imageUploadResult?.additionalImages?.length &&
+      imageUploadResult?.additionalImages?.length !== 2
+    ) {
+      throw new CustomError({
+        status: 400,
+        name: "BadRequest",
+        message: constants.response.IMAGES_REQUIRED,
+      });
+    }
+
     // Validate parameters
     await validatorId(req);
 
@@ -183,9 +215,33 @@ const updateWebTemplateById = async (req, res) => {
     const { name, link, description, price, events } = req.body;
 
     // Find the existing record
-    let webTemplateModel = await WebTemplate.findByPk(req.params.id, {
-      transaction,
-    });
+    let webTemplateModel = await WebTemplate.findOne(
+      {
+        where: {
+          id: req.params.id,
+        },
+        include: [
+          {
+            model: Image,
+            as: "ThumbnailImage",
+            attributes: ["id", "link"],
+            required: false,
+          },
+          {
+            model: Image,
+            as: "Images",
+            required: true,
+            attributes: ["id", "link"],
+            where: {
+              is_thumbnail: false,
+            },
+          },
+        ],
+      },
+      {
+        transaction,
+      }
+    );
 
     if (!webTemplateModel)
       throw new CustomError({
@@ -206,63 +262,70 @@ const updateWebTemplateById = async (req, res) => {
       transaction,
     });
 
-    // Update existing images
-    let existingImages = await Image.findAll({
-      where: { web_template: webTemplateModel.id },
-      transaction,
-    });
+    // Update images
+    if (imageUploadResult?.mainImage) {
+      // Create new thumbnail image
+      const newThumbnailImageModel = await Image.create(
+        {
+          link: imageUploadResult.mainImage,
+          web_template: webTemplateModel.id,
+          is_thumbnail: true,
+        },
+        {
+          transaction,
+        }
+      );
 
-    // Delete images that will no longer be used
-    const imagesToDelete = existingImages.filter(
-      (img) =>
-        img.is_thumbnail ||
-        !imageUploadResult.additionalImages.includes(img.link)
-    );
+      const oldThumbnailImageModel = webTemplateModel.ThumbnailImage; // Old thumbnail image
 
-    for (const image of imagesToDelete) {
-      await deleteFile(image.link);
-      await image.destroy({ transaction });
-    }
+      // Update the web template to use the new thumbnail image
+      webTemplateModel = await webTemplateModel.update(
+        {
+          image: newThumbnailImageModel.id,
+        },
+        { transaction }
+      );
 
-    // Create or update the main image
-    if (imageUploadResult.mainImage) {
-      const mainImage = existingImages.find((img) => img.is_thumbnail);
-      if (mainImage) {
-        await mainImage.update(
-          { link: imageUploadResult.mainImage },
-          { transaction }
-        );
-      } else {
-        const imageModel = await Image.create(
-          {
-            web_template: webTemplateModel.id,
-            link: imageUploadResult.mainImage,
-            is_thumbnail: true,
-          },
-          { transaction }
-        );
-        webTemplateModel = await webTemplateModel.update(
-          { image: imageModel.id },
-          { transaction }
-        );
-      }
+      // Delete the old thumbnail image
+      await Image.destroy({
+        where: {
+          id: oldThumbnailImageModel.id,
+        },
+        transaction,
+      });
+
+      await deleteFile(oldThumbnailImageModel.link); // Delete the old thumbnail image file
     }
 
     // Create additional images
-    const newAdditionalImages = imageUploadResult.additionalImages.filter(
-      (imagePath) =>
-        !existingImages.some((img) => img.link === imagePath)
-    );
-
-    if (newAdditionalImages.length > 0) {
-      await Image.bulkCreate(
-        newAdditionalImages.map((imagePath) => ({
-          web_template: webTemplateModel.id,
-          link: imagePath,
-          is_thumbnail: false,
-        })),
+    if (imageUploadResult?.additionalImages?.length) {
+      // Create new additional images
+      const newAdditionalImagesModels = await Image.bulkCreate(
+        imageUploadResult.additionalImages.map((image) => {
+          return {
+            is_thumbnail: false,
+            web_template: webTemplateModel.id,
+            link: image,
+          };
+        }),
         { transaction }
       );
+
+      const oldAdditionalImages = webTemplateModel.Images; // Old additional images
+
+      // Delete the old additional images
+      await Image.destroy({
+        where: {
+          id: oldAdditionalImages.map((image) => image.id),
+          web_template: webTemplateModel.id,
+        },
+        transaction,
+      });
+
+      // Delete the old additional image files
+      for (const oldImage of oldAdditionalImages) {
+        await deleteFile(oldImage.link);
+      }
     }
 
     // Find existing events
@@ -348,6 +411,7 @@ const updateWebTemplateById = async (req, res) => {
         is_thumbnail: img.is_thumbnail,
       }))
     );
+
     webTemplateModel.setDataValue(
       "WebTemplateEvents",
       webTemplateEventModels || []
@@ -377,7 +441,6 @@ const updateWebTemplateById = async (req, res) => {
   }
 };
 
-
 /**
  * @param {Request} req
  * @param {Response} res
@@ -387,13 +450,24 @@ const getAllWebTemplates = async (req, res) => {
   try {
     let criteria = {
       attributes: ["id", "name", "price", "description", "link", "image"],
+      include: [
+        {
+          model: Image,
+          as: "ThumbnailImage",
+          attributes: ["link"],
+          required: false,
+        },
+      ],
     };
 
     const webTemplates = await WebTemplate.findAll(criteria);
 
     webTemplates.forEach((webTemplate) => {
-      if (webTemplate?.image)
-        webTemplate.setDataValue("image", getUrlPublicFile(webTemplate.image));
+      if (webTemplate?.ThumbnailImage?.link)
+        webTemplate.ThumbnailImage.setDataValue(
+          "link",
+          getUrlPublicFile(webTemplate.ThumbnailImage.link)
+        );
     });
 
     res.status(200).json({
@@ -419,7 +493,35 @@ const getWebTemplateById = async (req, res) => {
       where: {
         id: req.params.id,
       },
-      attributes: ["id", "name", "price", "description", "link", "image"],
+      attributes: ["id", "name", "price", "description", "link"],
+      include: [
+        {
+          model: Image,
+          as: "ThumbnailImage",
+          attributes: ["link"],
+          required: false,
+        },
+        {
+          model: Image,
+          as: "Images",
+          attributes: ["link"],
+          where: {
+            is_thumbnail: false,
+          },
+          required: false,
+        },
+        {
+          model: WebTemplateEvent,
+          attributes: ["event"],
+          include: [
+            {
+              model: Event,
+              attributes: ["name"],
+              required: true,
+            },
+          ],
+        },
+      ],
     });
 
     if (!webTemplateModel)
@@ -429,11 +531,17 @@ const getWebTemplateById = async (req, res) => {
         message: constants.response.WEB_TEMPLATE_NOT_FOUND,
       });
 
-    if (webTemplateModel?.image)
-      webTemplateModel.setDataValue(
-        "image",
-        getUrlPublicFile(webTemplateModel.image)
+    if (webTemplateModel?.ThumbnailImage?.link)
+      webTemplateModel.ThumbnailImage.setDataValue(
+        "link",
+        getUrlPublicFile(webTemplateModel.ThumbnailImage.link)
       );
+
+    if (webTemplateModel?.Images?.length) {
+      webTemplateModel?.Images.forEach((image) => {
+        image.setDataValue("link", getUrlPublicFile(image.link));
+      });
+    }
 
     res.status(200).json({
       status: "OK",
@@ -458,7 +566,7 @@ const deleteWebTemplateById = async (req, res) => {
     transaction = await Database.transaction();
 
     // Find the template by ID
-    const webTemplateModel = await WebTemplate.findByPk(req.params.id, {
+    let webTemplateModel = await WebTemplate.findByPk(req.params.id, {
       transaction,
     });
 
@@ -475,10 +583,13 @@ const deleteWebTemplateById = async (req, res) => {
       transaction,
     });
 
-    // Delete physical files of associated images
-    for (const image of associatedImages) {
-      await deleteFile(image.link);
-    }
+    // Update the template to remove the image
+    webTemplateModel = await webTemplateModel.update(
+      {
+        image: null,
+      },
+      { transaction }
+    );
 
     // Delete image records from the database
     await Image.destroy({
@@ -489,7 +600,13 @@ const deleteWebTemplateById = async (req, res) => {
     // Delete the template
     await webTemplateModel.destroy({ transaction });
 
+    // Commit the transaction
     await transaction.commit();
+
+    // Delete physical files of associated images
+    for (const image of associatedImages) {
+      await deleteFile(image.link);
+    }
 
     // Respond with success
     res.status(200).json({
